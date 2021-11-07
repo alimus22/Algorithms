92decfd0355e5be3afef043b57fcb9bef2ead4be
Ali Hanni <alihanni@MacBook-Pro-2.local>
2021-11-07 16:27:45 -0500

    Bubble sort algorithm done

--- BubbleSort/src/Main.java
+++ BubbleSort/src/Main.java
@@ -1,10 +1,54 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Unit tests");
+
         int[] numbers = {2, 6, 1, -3, 12, 23};
+        printArray(bubbleSort(numbers));
+
+        int[] a = {1};
+        printArray(bubbleSort(a));
+
+        int[] b = {};
+        printArray(bubbleSort(b));
+
+        int[] d = {1, 1, 1, 1};
+        printArray(d);
     }
 
+
     public static int[] bubbleSort(int[] a) {
+        boolean swapped;
+        int n = a.length;
+
+        do {
+           swapped = false;
+
+           for(int i = 1; i < n - 1; i++) {
+               if(a[i - 1] > a[i]) { //if pair is not ordered
+                  swap(a, i - 1, i);
+                  swapped = true;
+               }
+           }
+        } while (swapped);
+
         return a;
+    }
+
+    public static boolean swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        return true;
+    }
+
+    public static void printArray(int[] a) {
+        String str = "[";
+        for(int i: a) {
+            str += i + " ";
+        }
+        str += "]";
+        System.out.println(str);
     }
 }
