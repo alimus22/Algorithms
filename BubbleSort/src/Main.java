bf7c27154925f3df8ccbabf0e9ebc840231af4c3
Ali Hanni <alihanni@MacBook-Pro-2.local>
2021-11-07 15:48:26 -0500

    Started Bubble sort algorithm

---
+++ BubbleSort/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 6, 1, -3, 12, 23};
+    }
+
+    public static int[] bubbleSort(int[] a) {
+        return a;
+    }
+}
